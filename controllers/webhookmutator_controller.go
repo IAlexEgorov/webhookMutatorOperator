e5391a3812f842bd99ce538fe394b42abbe43eae
Егоров Александр <aegorov@neoflex.ru>
2023-04-19 01:12:51 +0300

    Change logic

--- controllers/webhookmutator_controller.go
+++ controllers/webhookmutator_controller.go
@@ -73,17 +73,17 @@ func (r *WebhookMutatorReconciler) Reconcile(ctx context.Context, req ctrl.Reque
 	decoder := factory.UniversalDeserializer()
 
 	webhookResources := WebhookResources{}
-	webhookResources.createConfigMap(decoder, operatorLogger)
-	//webhookResources.createSecret(decoder, operatorLogger)
+	webhookResources.declareConfigMap(decoder, operatorLogger)
+	webhookResources.declareSecret(decoder, operatorLogger)
 
-	webhookResources.createServiceAccount(decoder, operatorLogger)
-	webhookResources.createClusterRole(decoder, operatorLogger)
-	webhookResources.createClusterRoleBinding(decoder, operatorLogger)
+	webhookResources.declareServiceAccount(decoder, operatorLogger)
+	webhookResources.declareClusterRole(decoder, operatorLogger)
+	webhookResources.declareClusterRoleBinding(decoder, operatorLogger)
 
-	webhookResources.createDeployment(decoder, operatorLogger)
-	webhookResources.createService(decoder, operatorLogger)
+	webhookResources.declareDeployment(decoder, operatorLogger)
+	webhookResources.declareService(decoder, operatorLogger)
 
-	webhookResources.createMutatingWebhookConfiguration(decoder, operatorLogger)
+	webhookResources.declareMutatingWebhookConfiguration(decoder, operatorLogger)
 
 	return ctrl.Result{RequeueAfter: time.Duration(30 * time.Second)}, nil
 }
@@ -96,7 +96,7 @@ func compactValue(v string) string {
 	return compact.String()
 }
 
-func (w *WebhookResources) createConfigMap(decoder runtime.Decoder, operatorLogger logr.Logger) {
+func (w *WebhookResources) declareConfigMap(decoder runtime.Decoder, operatorLogger logr.Logger) {
 
 	input := fmt.Sprintf(`
 apiVersion: v1
@@ -128,7 +128,7 @@ data:
 	w.ConfigMap = obj.(*v12.ConfigMap)
 	operatorLogger.Info("ConfigMap has added")
 }
-func (w *WebhookResources) createDeployment(decoder runtime.Decoder, operatorLogger logr.Logger) {
+func (w *WebhookResources) declareDeployment(decoder runtime.Decoder, operatorLogger logr.Logger) {
 	input := fmt.Sprintf(`
 apiVersion: apps/v1
 kind: Deployment
@@ -185,7 +185,7 @@ spec:
 	w.Deployment = obj.(*v1.Deployment)
 	operatorLogger.Info("Deployment has added")
 }
-func (w *WebhookResources) createService(decoder runtime.Decoder, operatorLogger logr.Logger) {
+func (w *WebhookResources) declareService(decoder runtime.Decoder, operatorLogger logr.Logger) {
 	input := fmt.Sprintf(`
 apiVersion: v1
 kind: Service
@@ -211,7 +211,7 @@ spec:
 	w.Service = obj.(*v12.Service)
 	operatorLogger.Info("Service has added")
 }
-func (w *WebhookResources) createServiceAccount(decoder runtime.Decoder, operatorLogger logr.Logger) {
+func (w *WebhookResources) declareServiceAccount(decoder runtime.Decoder, operatorLogger logr.Logger) {
 	input := fmt.Sprintf(`
 apiVersion: v1
 kind: ServiceAccount
@@ -227,7 +227,7 @@ metadata:
 	w.ServiceAccount = obj.(*v12.ServiceAccount)
 	operatorLogger.Info("ServiceAccount has added")
 }
-func (w *WebhookResources) createClusterRole(decoder runtime.Decoder, operatorLogger logr.Logger) {
+func (w *WebhookResources) declareClusterRole(decoder runtime.Decoder, operatorLogger logr.Logger) {
 	input := fmt.Sprintf(`
 apiVersion: rbac.authorization.k8s.io/v1
 kind: ClusterRole
@@ -246,7 +246,7 @@ rules:
 	w.ClusterRole = obj.(*v13.ClusterRole)
 	operatorLogger.Info("ClusterRole has added")
 }
-func (w *WebhookResources) createClusterRoleBinding(decoder runtime.Decoder, operatorLogger logr.Logger) {
+func (w *WebhookResources) declareClusterRoleBinding(decoder runtime.Decoder, operatorLogger logr.Logger) {
 	input := fmt.Sprintf(`
 apiVersion: rbac.authorization.k8s.io/v1
 kind: ClusterRoleBinding
@@ -269,7 +269,7 @@ roleRef:
 	w.ClusterRoleBinding = obj.(*v13.ClusterRoleBinding)
 	operatorLogger.Info("ClusterRoleBinding has added")
 }
-func (w *WebhookResources) createSecret(decoder runtime.Decoder, operatorLogger logr.Logger) {
+func (w *WebhookResources) declareSecret(decoder runtime.Decoder, operatorLogger logr.Logger) {
 	input := fmt.Sprintf(`
 apiVersion: v1
 kind: Secret
@@ -277,60 +277,8 @@ metadata:
   name: aegorov-admission-tls
 type: Opaque
 data:
-  tls.crt: |
-    -----BEGIN CERTIFICATE-----
-    MIIENTCCAx2gAwIBAgIUUgD6UPvI3Wbjrf4Ikm4MINyCVCgwDQYJKoZIhvcNAQEL
-    BQAwTzELMAkGA1UEBhMCUlUxEDAOBgNVBAgTB0V4YW1wbGUxDzANBgNVBAcTBk1v
-    c2NvdzEQMA4GA1UEChMHRXhhbXBsZTELMAkGA1UECxMCQ0EwHhcNMjMwNDE3MTAy
-    NzAwWhcNNDMwNDEyMTAyNzAwWjBPMQswCQYDVQQGEwJSVTEQMA4GA1UECBMHRXhh
-    bXBsZTEPMA0GA1UEBxMGTW9zY293MRAwDgYDVQQKEwdFeGFtcGxlMQswCQYDVQQL
-    EwJDQTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBALQxrg4GXLb/njuy
-    u8+o7n5t54SedWnbbD60CSrkika0i3VYJnK6SMq2Z1Dk1P/T7yVRXrm2LH6z8ocO
-    s+A5Gf7LlUIqtrRxmO/9GiK7Yn7/vrkM8fMUs3CyX2RqIIAEcn+a/7C1MJeSWULr
-    JsbwEyMc2Xcbj1RikTzHbcRXAgSB0e4fzVEiiIGJZnXw4xcDdvS0gJ6dDi5oEYVR
-    hRzouJVQCa18E/fA+6VgVmGQxkmLm/bCcIGhd2/hFMwlOxNKJ5qCZvoB8YwW0uJ0
-    lUL5V5mqa9ymxPGdDzj31OQbFyWcUntqZFUH8hO+vHYKADguJiHNrrKcKIO7adNz
-    XQ20og8CAwEAAaOCAQcwggEDMA4GA1UdDwEB/wQEAwIFoDAdBgNVHSUEFjAUBggr
-    BgEFBQcDAQYIKwYBBQUHAwIwDAYDVR0TAQH/BAIwADAdBgNVHQ4EFgQUa4zr7Y3Q
-    Nr4LhjA2rE0LjFTMTNAwgaQGA1UdEQSBnDCBmYIrYWVnb3Jvdi1hZG1pc3Npb24u
-    ZGVmYXVsdC5zdmMuY2x1c3Rlci5sb2NhbIIRYWVnb3Jvdi1hZG1pc3Npb26CJ2Fl
-    Z29yb3YtYWRtaXNzaW9uLmRlZmF1bHQuY2x1c3Rlci5sb2NhbIIdYWVnb3Jvdi1h
-    ZG1pc3Npb24uZGVmYXVsdC5zdmOCCWxvY2FsaG9zdIcEfwAAATANBgkqhkiG9w0B
-    AQsFAAOCAQEATzVLyDdUOxgZrgl2d/QP8E+C6KBUen76sw7hsrYgh3xoOZWL7Mea
-    IxJvMJ0GgCcjINZzXHRUn4Ai4VPvmOXpsMoeMtkQdy3edIuGu/AtReg75S1vnZsX
-    OKUuv0FsgoSYmhx2kLAHzMinLESyeaDFuyPnvBUAY0pKCKMe87RACK7NwWkiJMv3
-    5xKdjGfmbD2OMQDhjahfz9ZulPEvruVyu2jtc/cQHAkQ4BKwGygWe0IPH5a3UMnY
-    GyzTNy25+V3zDkWeiqJQvP6zh3cWBl5tCOf+boX6VU7GLXvYYJ9YX5wZ4Y+JSM9t
-    DxPsByLVfC+YCHm1xuvjB8hpOFlPyqw3Fw==
-    -----END CERTIFICATE-----
-  tls.key: |
-    -----BEGIN RSA PRIVATE KEY-----
-    MIIEpAIBAAKCAQEAtDGuDgZctv+eO7K7z6jufm3nhJ51adtsPrQJKuSKRrSLdVgm
-    crpIyrZnUOTU/9PvJVFeubYsfrPyhw6z4DkZ/suVQiq2tHGY7/0aIrtifv++uQzx
-    8xSzcLJfZGoggARyf5r/sLUwl5JZQusmxvATIxzZdxuPVGKRPMdtxFcCBIHR7h/N
-    USKIgYlmdfDjFwN29LSAnp0OLmgRhVGFHOi4lVAJrXwT98D7pWBWYZDGSYub9sJw
-    gaF3b+EUzCU7E0onmoJm+gHxjBbS4nSVQvlXmapr3KbE8Z0POPfU5BsXJZxSe2pk
-    VQfyE768dgoAOC4mIc2uspwog7tp03NdDbSiDwIDAQABAoIBAC3b4ucw2VG9dmDN
-    GR09ag0FHYHT7h/VtxOyMA8ZgNODyWZA2/Ag1ru4dkzRICBHqLo/njL3WRWZ6GRU
-    6pRrE+GToFXplvwPWRPiv08Nj4Cwx7JCyCTMJOrOipZ8p+7MsvMk2GP5iPdaSJtO
-    S10f5k7uXa4BdpXMTBhCzP3GAv5wOlC88oMB2EQkFIlBliIAA/vGHIiPFxRKgWd8
-    /kCeG0wXnsiYGNl6q4b4eziUGe1z/BqMBJEPNjMLw2G3YgL17uWeLsIASUCoSpZs
-    Qx0V5DJVrpudJoUAZntGYHIMEokpJbh7xkamOlUiJKFq4Fm+/5fOFfRj7qgGPVIV
-    L1guo/kCgYEA1ReStbYR/ogI7njW5MY+5XKfdZLMDeYUVv053A+Gv99BsLNzrrN4
-    3yHs+ePaE90AN9HZWyjAklH6jPeuKVcgsYobtxxhpgH9WCqxc2KlrsCMSG1Cj93W
-    bnui7BfpG01vA16DISchRGHAOcWKdGTl2chnHmPDFW3MO7RGMbgENuMCgYEA2HpJ
-    UvmXZz2o6tZgAglFhyaqCwVAwpcqABd9GoAEJYaw/8ByT0tB4TY7QlIabLeho19X
-    RQDnSpkayUD7zvp+Xz9d4u2G0+m6g3iPD76DfzgY1CO2A3fVvSenZbLJCUFtb/7P
-    +3LW5n683jRtaFxDgRcVhszFGvHhxodizmSfo+UCgYBm/L+hJt/HfoOijbB+XPnk
-    9uLudgY86WgHKted7bsYXJRpDDHqyz6tyL25gE/Trcn/MGK2VhnMHebT3pjDziI0
-    7CS8+PCQxQFmbSvaMmEU9mZWUsgKtBKAzyxMorm8wk3W+QSzLzA61muLAFgMLP+R
-    o/OCkoCkiK5eZKBQQzl2NQKBgQCHiMivsqUgdnJz1ZR2sedeHs8H51oMeyhItmwV
-    U2FFPXdEKPFow+2TW6judQkmWDJTXzX8dfxZsFIc/xqpFBxa9gmKMrzfoM6t0aWA
-    bf9Wf4DMES+8LCMeMt2TxsQnj1c9b4Q4IkZ6OY92b8ywMlPxVsqbg0lE/XwmGE28
-    ezOYIQKBgQCWBr2iqvsMx4PFZMzl6exy1hQgYAdRc5BStaFEqAggW37q3l5rXn5m
-    O0/9JWSQUPhi/mcBWko4hEWkwWAsqaR3bzzyPAtboVwm/jc1jbM3VA+xMbiVHXTN
-    sxph/gHtsyHwccvrx94d1YN1/oLO14KcvK3fxRpVamlzN5e32oZo2g==
-    -----END RSA PRIVATE KEY-----`)
+  tls.crt: LS0tLS1CRUdJTiBDRVJUSUZJQ0FURS0tLS0tCk1JSURiakNDQWxhZ0F3SUJBZ0lVWHVMYW1NSms5bEhORjZEOGpudkZxZHMxNDhvd0RRWUpLb1pJaHZjTkFRRUwKQlFBd1R6RUxNQWtHQTFVRUJoTUNVbFV4RURBT0JnTlZCQWdUQjBWNFlXMXdiR1V4RHpBTkJnTlZCQWNUQmsxdgpjMk52ZHpFUU1BNEdBMVVFQ2hNSFJYaGhiWEJzWlRFTE1Ba0dBMVVFQ3hNQ1EwRXdIaGNOTWpNd05ERTNNVEF5Ck56QXdXaGNOTWpnd05ERTFNVEF5TnpBd1dqQlBNUXN3Q1FZRFZRUUdFd0pTVlRFUU1BNEdBMVVFQ0JNSFJYaGgKYlhCc1pURVBNQTBHQTFVRUJ4TUdUVzl6WTI5M01SQXdEZ1lEVlFRS0V3ZEZlR0Z0Y0d4bE1Rc3dDUVlEVlFRTApFd0pEUVRDQ0FTSXdEUVlKS29aSWh2Y05BUUVCQlFBRGdnRVBBRENDQVFvQ2dnRUJBTWlGd2dlUGFrdC91dy9RCjl1N21pWmpndG9nWXl3U0xZcTN6c0I0RFQyK0twUEVpbTFVVFRCVHZzVWNnYTk5cUt5bTFxZXF3WWJSa3RIZHUKZGwzOHZTSEY0K0lOYmFpem1mY1hrSTFVV3I4dmFHaHVCc0lRd2lWdm9UODFialFSTk1MbWNma2dYM09BakJQSgo5U2UwSnpjbGY0dUVEd2R4R0xzdnJieElKWGk1UmxZVjZwekFiUUF0UE5pYWc0aExDaFpqY1FmRW1Cc1oyMjBkCncxMDZzeFVmRWplYjZoRWVvYnhjTHdzcTlGY00ySGJXMm8xYmtDY3ZuSm4ySEYzNXRlSmlDbHFBRWczaFpQOEYKOFdQKzNXREVHUVV2eFdWSFJtaEZqb0RNRFdDV1QzWkZZaVQvZU12c1l6c3duM2tpS2dON29jWnhXNXJvN3ZFQQpEeDI4K3ljQ0F3RUFBYU5DTUVBd0RnWURWUjBQQVFIL0JBUURBZ0VHTUE4R0ExVWRFd0VCL3dRRk1BTUJBZjh3CkhRWURWUjBPQkJZRUZQdDUzT0s3R0FMd3RidlVWNXorNXlvN2ZTU1lNQTBHQ1NxR1NJYjNEUUVCQ3dVQUE0SUIKQVFBWnhwcEs0eXhSVW1sckM5Q3BDR3M0VzR5LzNmTUtCVTRPT2Y3Q1R3VlIzQVc2bEdUbXYvTitCM01Wd3d1OQpESjJsYmhiTUhpSGdnMUdSd1IvN0t5T2FmeFQ0YkNPUG9NUjBZRU1Db0J6R3pRNHIvRUp1aStyRk03RGY1Mnh2ClJSOHprTnJIcXk5KzB1b1JackltRnNqYWNKOFBhUEdsZmV0eWFISEVGTDFNSXZkeGtEZ0xGYVBlcTZBaVJhT3AKd0VZenVhUnRsRDNUbVVSSXN1Yk9tN3Bpc1lITUN6NFdUeERzd2QzVU9OUGxpQlU2ZkVsZzZkNVFlMDVyWGc4YQpTMURnQThFKzRJZHkralZ0dk9YcmJTa3d0RDRYY2w3RWppQXk5TVY5TGlnelNCdmFReXlvb3FGR3FMYU43WDV3CkNtZVlxLzNHNUpVb2wzR3J5bENkekNDUQotLS0tLUVORCBDRVJUSUZJQ0FURS0tLS0t
+  tls.key: LS0tLS1CRUdJTiBSU0EgUFJJVkFURSBLRVktLS0tLQpNSUlFcEFJQkFBS0NBUUVBdERHdURnWmN0ditlTzdLN3o2anVmbTNuaEo1MWFkdHNQclFKS3VTS1JyU0xkVmdtCmNycEl5clpuVU9UVS85UHZKVkZldWJZc2ZyUHlodzZ6NERrWi9zdVZRaXEydEhHWTcvMGFJcnRpZnYrK3VRengKOHhTemNMSmZaR29nZ0FSeWY1ci9zTFV3bDVKWlF1c214dkFUSXh6WmR4dVBWR0tSUE1kdHhGY0NCSUhSN2gvTgpVU0tJZ1lsbWRmRGpGd04yOUxTQW5wME9MbWdSaFZHRkhPaTRsVkFKclh3VDk4RDdwV0JXWVpER1NZdWI5c0p3CmdhRjNiK0VVekNVN0Uwb25tb0ptK2dIeGpCYlM0blNWUXZsWG1hcHIzS2JFOFowUE9QZlU1QnNYSlp4U2UycGsKVlFmeUU3NjhkZ29BT0M0bUljMnVzcHdvZzd0cDAzTmREYlNpRHdJREFRQUJBb0lCQUMzYjR1Y3cyVkc5ZG1ETgpHUjA5YWcwRkhZSFQ3aC9WdHhPeU1BOFpnTk9EeVdaQTIvQWcxcnU0ZGt6UklDQkhxTG8vbmpMM1dSV1o2R1JVCjZwUnJFK0dUb0ZYcGx2d1BXUlBpdjA4Tmo0Q3d4N0pDeUNUTUpPck9pcFo4cCs3TXN2TWsyR1A1aVBkYVNKdE8KUzEwZjVrN3VYYTRCZHBYTVRCaEN6UDNHQXY1d09sQzg4b01CMkVRa0ZJbEJsaUlBQS92R0hJaVBGeFJLZ1dkOAova0NlRzB3WG5zaVlHTmw2cTRiNGV6aVVHZTF6L0JxTUJKRVBOak1MdzJHM1lnTDE3dVdlTHNJQVNVQ29TcFpzClF4MFY1REpWcnB1ZEpvVUFabnRHWUhJTUVva3BKYmg3eGthbU9sVWlKS0ZxNEZtKy81Zk9GZlJqN3FnR1BWSVYKTDFndW8va0NnWUVBMVJlU3RiWVIvb2dJN25qVzVNWSs1WEtmZFpMTURlWVVWdjA1M0ErR3Y5OUJzTE56cnJONAozeUhzK2VQYUU5MEFOOUhaV3lqQWtsSDZqUGV1S1ZjZ3NZb2J0eHhocGdIOVdDcXhjMktscnNDTVNHMUNqOTNXCmJudWk3QmZwRzAxdkExNkRJU2NoUkdIQU9jV0tkR1RsMmNobkhtUERGVzNNTzdSR01iZ0VOdU1DZ1lFQTJIcEoKVXZtWFp6Mm82dFpnQWdsRmh5YXFDd1ZBd3BjcUFCZDlHb0FFSllhdy84QnlUMHRCNFRZN1FsSWFiTGVobzE5WApSUURuU3BrYXlVRDd6dnArWHo5ZDR1MkcwK202ZzNpUEQ3NkRmemdZMUNPMkEzZlZ2U2VuWmJMSkNVRnRiLzdQCiszTFc1bjY4M2pSdGFGeERnUmNWaHN6Rkd2SGh4b2Rpem1TZm8rVUNnWUJtL0wraEp0L0hmb09pamJCK1hQbmsKOXVMdWRnWTg2V2dIS3RlZDdic1lYSlJwRERIcXl6NnR5TDI1Z0UvVHJjbi9NR0syVmhuTUhlYlQzcGpEemlJMAo3Q1M4K1BDUXhRRm1iU3ZhTW1FVTltWldVc2dLdEJLQXp5eE1vcm04d2szVytRU3pMekE2MW11TEFGZ01MUCtSCm8vT0Nrb0NraUs1ZVpLQlFRemwyTlFLQmdRQ0hpTWl2c3FVZ2RuSnoxWlIyc2VkZUhzOEg1MW9NZXloSXRtd1YKVTJGRlBYZEVLUEZvdysyVFc2anVkUWttV0RKVFh6WDhkZnhac0ZJYy94cXBGQnhhOWdtS01yemZvTTZ0MGFXQQpiZjlXZjRETUVTKzhMQ01lTXQyVHhzUW5qMWM5YjRRNElrWjZPWTkyYjh5d01sUHhWc3FiZzBsRS9Yd21HRTI4CmV6T1lJUUtCZ1FDV0JyMmlxdnNNeDRQRlpNemw2ZXh5MWhRZ1lBZFJjNUJTdGFGRXFBZ2dXMzdxM2w1clhuNW0KTzAvOUpXU1FVUGhpL21jQldrbzRoRVdrd1dBc3FhUjNienp5UEF0Ym9Wd20vamMxamJNM1ZBK3hNYmlWSFhUTgpzeHBoL2dIdHN5SHdjY3ZyeDk0ZDFZTjEvb0xPMTRLY3ZLM2Z4UnBWYW1sek41ZTMyb1pvMmc9PQotLS0tLUVORCBSU0EgUFJJVkFURSBLRVktLS0tLQ==`)
 
 	obj, _, err := decoder.Decode([]byte(input), nil, nil)
 	if err != nil {
@@ -340,7 +288,7 @@ data:
 	w.Secret = obj.(*v12.Secret)
 	operatorLogger.Info("Secret has added")
 }
-func (w *WebhookResources) createMutatingWebhookConfiguration(decoder runtime.Decoder, operatorLogger logr.Logger) {
+func (w *WebhookResources) declareMutatingWebhookConfiguration(decoder runtime.Decoder, operatorLogger logr.Logger) {
 	input := fmt.Sprintf(`
 apiVersion: admissionregistration.k8s.io/v1
 kind: MutatingWebhookConfiguration
